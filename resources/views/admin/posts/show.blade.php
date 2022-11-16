8f6f633dd20bfe48fda63cbad8beaa4f3e64fc27
Denis-92 <d.mozenic@gmail.com>
2022-11-16 17:58:22 +0100

    add delete function

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,4 +7,12 @@
     <div class="mt-5">
         <a href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
     </div>
+
+    <div class="mt-1">
+        <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="Cancella" onclick="confirm('Are you sure?')">
+        </form>
+    </div>
 @endsection
