f6a6ed8c38107aed2b5e788a65e662c9d02aa091
Denis-92 <d.mozenic@gmail.com>
2022-11-16 12:30:13 +0100

    add guest homepage

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,25 +1,19 @@
-{{-- @extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Token CSRF -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Front Office') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
+<body>
 
-GUEST!
+    test
+</body>
+
+</html>
