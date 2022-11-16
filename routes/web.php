8c58d3e83427e390df2a1799600a07e685aa877a
Denis-92 <d.mozenic@gmail.com>
2022-11-16 16:05:13 +0100

    fix controller

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware('auth')
     ->group(function () {
         Route::get('/', 'HomeController@index')
         ->name('home');
+        Route::resource('posts', 'PostController');
     });
 
 Route::get("{any?}", function() {
