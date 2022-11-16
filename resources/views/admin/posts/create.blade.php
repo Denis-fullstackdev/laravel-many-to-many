29d4f1c9276a72db59b4ea1e97dab5aefb36b606
Denis-92 <d.mozenic@gmail.com>
2022-11-16 18:22:04 +0100

    add error message in: edit, create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,15 +1,29 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    @if ($errors->any())
+        <div class="row mb-2">
+            <div class="col-12 bg-danger">
+                Correggere gli errori!
+            </div>
+        </div>
+    @endif
+
     <form method="POST" action="{{ route('admin.posts.store') }}">
         @csrf
-        <div>
+        <div @error('title') class='is-invalid' @enderror>
             <label for="title">Titolo</label>
             <input type="text" name="title" required minlength="5" maxlength="255" value="{{ old('title', '') }}">
+            @error('title')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror>
         </div>
-        <div>
+        <div @error('content') class='is-invalid' @enderror>
             <label for="content">Contenuto</label>
             <textarea name="content" cols="30" rows="10" required>{{ old('title', '') }}</textarea>
+            @error('content')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror>
         </div>
         <div>
             <input type="submit" value="Invia">
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    @if ($errors->any())
+        <div class="row mb-2">
+            <div class="col-12 bg-danger">
+                Correggere gli errori!
+            </div>
+        </div>
+    @endif
+
     <form method="POST" action="{{ route('admin.posts.update', $post->id) }}">
         @csrf
         @method('PATCH')
-        <div>
+        <div @error('title') class='is-invalid' @enderror>
             <label for="title">Titolo</label>
             <input type="text" name="title" required minlength="5" maxlength="255"
                 value="{{ old('title', $post->title) }}">
+            @error('title')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror>
         </div>
-        <div>
+        <div @error('content') class='is-invalid' @enderror>
             <label for="content">Contenuto</label>
             <textarea name="content" cols="30" rows="10" required>{{ old('title', $post->content) }}</textarea>
+            @error('content')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror>
         </div>
         <div>
             <input type="submit" value="Aggiorna">
