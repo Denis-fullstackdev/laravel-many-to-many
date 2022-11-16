1f8b12fe3d580662ffe9f4cdfb05c6fbf32c181b
Denis-92 <d.mozenic@gmail.com>
2022-11-16 23:08:54 +0100

    add categories to edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -20,6 +20,24 @@
                 <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
+
+        {{-- categories --}}
+        <div>
+            <label for="category_id">Categoria</label>
+            <select name="category_id">
+                <option value="">Nessuna categoria</option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}"
+                        {{ $category->id == old('category_id', $post->category_id) ? 'selected' : '' }}>
+                        {{ $category->name }}
+                    </option>
+                @endforeach
+            </select>
+            @error('category_id')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
+        </div>
+
         <div @error('content') class='is-invalid' @enderror>
             <label for="content">Contenuto</label>
             <textarea name="content" cols="30" rows="10" required>{{ old('title', $post->content) }}</textarea>
