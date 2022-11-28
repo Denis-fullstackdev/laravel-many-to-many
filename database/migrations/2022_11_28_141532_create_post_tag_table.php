63b4e6975e6082f8389a4a93cc1285307ec7d536
Denis-92 <d.mozenic@gmail.com>
2022-11-28 15:24:23 +0100

    make migration pivot table

--- database/migrations/2022_11_28_141532_create_post_tag_table.php
+++ database/migrations/2022_11_28_141532_create_post_tag_table.php
@@ -14,8 +14,17 @@ class CreatePostTagTable extends Migration
     public function up()
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            // $table->id();
+            // $table->timestamps();
+            // le due precedenti righe non servono per tabella pivot pura, ma servono in caso esistono, nella tabella, altre colonne oltre alle due foreign key
+
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags');
+
+            $table->primary(['post_id', 'tag_id']);
         });
     }
 
