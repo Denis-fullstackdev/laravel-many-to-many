fa00712ef0fec0398d489f96caf86ba9b5d3d097
Denis-92 <d.mozenic@gmail.com>
2022-11-28 18:21:56 +0100

    add tags to store function

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -51,9 +51,13 @@ class PostController extends Controller
         $post = new Post();
         $post->fill($form_data);
 
-        $slug = $this->getSlug($post->title);
+        if(array_key_exists('tags', $form_data)){
+            $post->tags()->sync($form_data['tags']);
+        }
 
+        $slug = $this->getSlug($post->title);
         $post->slug = $slug;
+
         $post->save();
 
         return redirect()->route('admin.posts.show', $post->id);
